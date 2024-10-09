8c98964fe16af397d77bd5e0a0b6398873d11e3a
Nguyen Phi Hung <josephkirk.art@gmail.com>
2024-10-09 18:14:08 +0700

    Update WindSystemComponent.h

--- Source/JK_WindSystem/Public/WindSystemComponent.h
+++ Source/JK_WindSystem/Public/WindSystemComponent.h
@@ -1,19 +1,50 @@
+// WindSystemComponent.h
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "HAL/Runnable.h"
+#include "HAL/ThreadSafeBool.h"
 #include "WindSystemComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnWindCellUpdated, const FVector&, CellCenter, const FVector&, WindVelocity, float, CellSize);
 
+class FWindSimulationWorker : public FRunnable
+{
+public:
+    FWindSimulationWorker(class UWindSimulationComponent* InOwner);
+    virtual uint32 Run() override;
+    void Stop();
+
+private:
+    class UWindSimulationComponent* Owner;
+    FThreadSafeBool bShouldRun;
+};
+
+USTRUCT()
+struct FAdaptiveGridCell
+{
+    GENERATED_BODY()
+
+    FVector Velocity;
+    TArray<FAdaptiveGridCell*> Children;
+    float SubdivisionThreshold;
+    float MergeThreshold;
+
+    FAdaptiveGridCell() : Velocity(FVector::ZeroVector), SubdivisionThreshold(1.0f), MergeThreshold(0.1f) {}
+};
+
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class JK_WINDSYSTEM_API UWindSimulationComponent : public UActorComponent
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
+
 public:
     UWindSimulationComponent();
 
     virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+    virtual void BeginPlay() override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
     UFUNCTION(BlueprintCallable, Category = "Wind Simulation")
     FVector GetWindVelocityAtLocation(const FVector& Location) const;
@@ -21,49 +52,37 @@ public:
     UFUNCTION(BlueprintCallable, Category = "Wind Simulation")
     void AddWindAtLocation(const FVector& Location, const FVector& WindVelocity);
 
+    UFUNCTION(BlueprintCallable, Category = "Wind Simulation")
+    float GetSimulationFrequency() const { return SimulationFrequency; }
+
     UPROPERTY(BlueprintAssignable, Category = "Wind Simulation|Debug")
     FOnWindCellUpdated OnWindCellUpdated;
 
-protected:
-    virtual void BeginPlay() override;
-    //virtual void EndPlay() override;
+    //UPROPERTY(EditAnywhere, Category = "Wind Simulation")
+    //float SimulationFrequency;
+
+    void SimulationStep(float DeltaTime);
 
 private:
-    // Grid dimensions
-    UPROPERTY(EditAnywhere, Category = "Wind Simulation")
-    int32 GridSizeX = 256;
 
-    UPROPERTY(EditAnywhere, Category = "Wind Simulation")
-    int32 GridSizeY = 256;
+    TArray<FAdaptiveGridCell> AdaptiveGrid;
+    int32 BaseGridSize;
+    float CellSize;
+    float Viscosity;
+    float SimulationFrequency;
 
-    UPROPERTY(EditAnywhere, Category = "Wind Simulation")
-    int32 GridSizeZ = 4;
+    FWindSimulationWorker* SimulationWorker;
+    FRunnableThread* SimulationThread;
 
-    // Simulation parameters
-    UPROPERTY(EditAnywhere, Category = "Wind Simulation")
-    float CellSize = 128.0f; // Size of each grid cell in world units
-
-    UPROPERTY(EditAnywhere, Category = "Wind Simulation")
-    float SimulationFrequency = 1.0f; // How often to update the simulation (in seconds)
-
-    UPROPERTY(EditAnywhere, Category = "Wind Simulation")
-    float Viscosity = 0.1f;
-
-    // Wind grid
-    TArray<FVector> VelocityGrid;
-    TArray<FVector> PreviousVelocityGrid;
-
-    float SimulationTimer = 0.0f;
-
-private:
-    void NotifyCellUpdated(int32 X, int32 Y, int32 Z);
     void InitializeGrid();
-    void StepSimulation(float DeltaTime);
-    void AddSources();
+    void UpdateAdaptiveGrid();
+    void Subdivide(FAdaptiveGridCell& Cell);
+    void Merge(FAdaptiveGridCell& ParentCell);
     void Diffuse(TArray<FVector>& Dst, const TArray<FVector>& Src, float Diff, float Dt);
     void Project(TArray<FVector>& VelocityX, TArray<FVector>& VelocityY, TArray<FVector>& VelocityZ, TArray<FVector>& P, TArray<FVector>& Div);
+    void SetBoundary(TArray<FVector>& Field);
+    int32 IX(int32 x, int32 y, int32 z) const;
     void Advect(TArray<FVector>& Dst, const TArray<FVector>& Src, const TArray<FVector>& Velocity, float Dt);
-    int32 IX(int32 X, int32 Y, int32 Z) const;
-    // function to get the grid cell for a given world location
-    void GetGridCell(const FVector& Location, int32& OutX, int32& OutY, int32& OutZ) const;
+
+    void ApplySIMDOperations(TArray<FVector>& Vectors, float Scalar);
 };
