76bedb7c33898c140fbadb06fdf25ec58bddd3b5
Nguyen Phi Hung <josephkirk.art@gmail.com>
2024-10-09 19:22:23 +0700

    Update WindSystemVisualizer.h

--- Source/JK_WindSystem/Public/WindSystemVisualizer.h
+++ Source/JK_WindSystem/Public/WindSystemVisualizer.h
@@ -3,57 +3,37 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
 #include "Components/ActorComponent.h"
 #include "WindSystemComponent.h"
+#include "DrawDebugHelpers.h"
 #include "WindSystemVisualizer.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
-class UWindDebugVisualizer : public UActorComponent
+class UWindSimulationSubsystem;
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class JK_WINDSYSTEM_API UWindDebugVisualizer : public UActorComponent
 {
     GENERATED_BODY()
 
-public:    
+public:
     UWindDebugVisualizer();
 
     virtual void BeginPlay() override;
     virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
     virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-    UPROPERTY(EditAnywhere, Category = "Debug")
-    float ArrowScale = 100.0f;
-
-    UPROPERTY(EditAnywhere, Category = "Debug")
-    float ArrowThickness = 2.0f;
-
-    UPROPERTY(EditAnywhere, Category = "Debug")
-    FColor ArrowColor = FColor::Cyan;
-
-    UPROPERTY(EditAnywhere, Category = "Debug")
-    float DebugDrawDuration = 0.0f;
-
-    UPROPERTY(EditAnywhere, Category = "Debug")
-    float MinVelocityThreshold = 0.1f;
-
-    UPROPERTY(EditAnywhere, Category = "Debug")
-    bool bDrawArrows = true;
-
-    UPROPERTY(EditAnywhere, Category = "Debug")
-    bool bDrawVelocityText = false;
-
-    UPROPERTY(EditAnywhere, Category = "Debug")
-    bool bDrawAdaptiveGrid = true;
-
-    UPROPERTY(EditAnywhere, Category = "Debug")
-    float TextScale = 1.0f;
-
-    UPROPERTY(EditAnywhere, Category = "Debug")
-    int32 VisualizationResolution = 5;
+    // These properties are now controlled by the WindDebugVisualizationActor
+    float ArrowScale;
+    float ArrowThickness;
+    FLinearColor ArrowColor;
+    float MinVelocityThreshold;
+    bool bDrawArrows;
+    bool bDrawVelocityText;
+    bool bDrawAdaptiveGrid;
+    float TextScale;
+    int32 VisualizationResolution;
 
 private:
-    UFUNCTION()
-    void OnWindCellUpdated(const FVector& CellCenter, const FVector& WindVelocity, float CellSize);
-
     void DrawDebugArrow(const UWorld* World, const FVector& Start, const FVector& End, const FVector& Velocity);
     void DrawDebugVelocityText(const UWorld* World, const FVector& Location, const FVector& Velocity);
     void DrawDebugAdaptiveGrid(const UWorld* World);
@@ -83,7 +63,7 @@ public:
     UWindDebugVisualizer* WindDebugVisualizer;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wind Debug")
-    float ArrowScale = 100.0f;
+    float ArrowScale = 1.0f;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Wind Debug")
     float ArrowThickness = 2.0f;
