9f050b64dcdc345820dfc741b2d44ee5ea1c5474
Nguyen Phi Hung <josephkirk.art@gmail.com>
2024-10-18 10:01:31 +0700

    implement GPU read back

--- Source/JK_WindSystem/Private/WindSystemGPUComponent.cpp
+++ Source/JK_WindSystem/Private/WindSystemGPUComponent.cpp
@@ -8,7 +8,9 @@
 #include "ShaderCompilerCore.h"
 #include "RHIDefinitions.h"
 #include "RHIStaticStates.h"
+#include "WindSystemCommon.h"
 #include "DataDrivenShaderPlatformInfo.h"
+#include "RHIGPUReadback.h"
 
 class FWindSimulationCS : public FGlobalShader
 {
@@ -33,7 +35,18 @@ class FWindSimulationCS : public FGlobalShader
 IMPLEMENT_GLOBAL_SHADER(FWindSimulationCS, "/Plugin/JK_WindSystem/Private/WindSimulationCS.usf", "MainCS", SF_Compute);
 
 UWindGPUSimulationComponent::UWindGPUSimulationComponent()
+    : VelocityRenderTarget(nullptr), DensityRenderTarget(nullptr)
 {
+    //VelocityRenderTarget = NewObject<UTextureRenderTargetVolume>(this);
+    //VelocityRenderTarget->Init(GridSize, GridSize, GridSize, PF_FloatRGBA);
+    //VelocityRenderTarget->UpdateResource();
+
+    //DensityRenderTarget = NewObject<UTextureRenderTargetVolume>(this);
+    //DensityRenderTarget->Init(GridSize, GridSize, GridSize, PF_R32_FLOAT);
+    //DensityRenderTarget->UpdateResource();
+
+    LastDeltaTime = 0;
+
     PrimaryComponentTick.bCanEverTick = true;
     bAutoActivate = true;
 }
@@ -56,10 +69,23 @@ void UWindGPUSimulationComponent::BeginPlay()
         DensityRenderTarget->Init(GridSize, GridSize, GridSize, PF_R32_FLOAT);
         DensityRenderTarget->UpdateResource();
     }
+
+    // Start the timer to update the WindGrid
+    if (UWorld* World = GetWorld())
+    {
+        World->GetTimerManager().SetTimer(UpdateWindGridTimerHandle, this, &UWindGPUSimulationComponent::UpdateWindGridFromGPU_Callback, UpdateInterval, true);
+    }
 }
 
 void UWindGPUSimulationComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
+    // Clear the timer
+    if (UWorld* World = GetWorld())
+    {
+        World->GetTimerManager().ClearTimer(UpdateWindGridTimerHandle);
+    }
+
+
     Super::EndPlay(EndPlayReason);
 }
 
@@ -83,6 +109,101 @@ void UWindGPUSimulationComponent::AddWindAtLocation(const FVector& Location, con
 void UWindGPUSimulationComponent::SimulationStep(float DeltaTime)
 {
     Execute(VelocityRenderTarget, DensityRenderTarget, DeltaTime);
+}
+
+void UWindGPUSimulationComponent::UpdateWindGridFromGPU_Callback()
+{
+    //UpdateWindGridFromGPU();
+}
+void UWindGPUSimulationComponent::UpdateWindGridFromGPU()
+{
+    ENQUEUE_RENDER_COMMAND(UpdateWindGridFromGPU)(
+        [this](FRHICommandListImmediate& RHICmdList)
+        {
+            UpdateWindGridFromGPU_RenderThread(RHICmdList);
+        });
+}
+
+//TODO: Rewrite this part as this crash game if used. considering reference MedialOFrameManager.h or AddReadbackBufferPass in RenderGraphUtils.h 
+void UWindGPUSimulationComponent::UpdateWindGridFromGPU_RenderThread(FRHICommandListImmediate& RHICmdList)
+{
+    check(IsInRenderingThread());
+
+    if (!VelocityRenderTarget || !WindGrid)
+    {
+        UE_LOG(LogTemp, Error, TEXT("VelocityRenderTarget or WindGrid is null"));
+        return;
+    }
+
+    FRDGBuilder GraphBuilder(RHICmdList);
+    {
+        FRDGTextureRef SourceTexture = GraphBuilder.RegisterExternalTexture(CreateRenderTarget(VelocityRenderTarget->GetRenderTargetResource()->GetRenderTargetTexture(), TEXT("VelocityField")));
+
+        FIntVector Size = WindGrid->GetBoundSize();
+
+        // Create readback buffer
+        FRDGBufferDesc Desc;
+        Desc.BytesPerElement = sizeof(FFloat16Color);
+        Desc.NumElements = GridSize * GridSize * GridSize;
+        Desc.Usage = EBufferUsageFlags::StructuredBuffer | EBufferUsageFlags::ShaderResource | EBufferUsageFlags::UnorderedAccess;
+
+        TRefCountPtr<FRDGPooledBuffer> ReadbackBuffer = new FRDGPooledBuffer(
+            RHICmdList,
+            TEXT("WindSimulationReadbackBuffer"),
+            Desc,
+            ERDGBufferFlags::None,
+            ERHIAccess::UAVCompute
+        );
+
+        FRDGBufferRef RDGReadbackBuffer = GraphBuilder.RegisterExternalBuffer(ReadbackBuffer);
+
+        // Add a pass to copy texture data to the readback buffer
+        GraphBuilder.AddPass(
+            RDG_EVENT_NAME("CopyTextureToBuffer"),
+            ERDGPassFlags::Copy,
+            [this, SourceTexture, RDGReadbackBuffer, Size](FRHICommandList& RHICmdList)
+            {
+                FRHICopyTextureInfo CopyInfo;
+                CopyInfo.Size = FIntVector(Size.X, Size.Y, Size.Z);
+                RHICmdList.CopyTexture(SourceTexture->GetRHI(), RDGReadbackBuffer->GetRHI(), CopyInfo);
+            }
+        );
+
+        // Add a pass to read the buffer data and update the WindGrid
+        GraphBuilder.AddPass(
+            RDG_EVENT_NAME("UpdateWindGrid"),
+            ERDGPassFlags::Readback,
+            [this, RDGReadbackBuffer, Size](FRHICommandList& RHICmdList)
+            {
+                void* Data = RHICmdList.LockBuffer(RDGReadbackBuffer->GetRHI(), 0, RDGReadbackBuffer->Desc.GetSize(), RLM_ReadOnly);
+                if (Data)
+                {
+                    FFloat16Color* ColorData = static_cast<FFloat16Color*>(Data);
+
+                    for (int32 Z = 0; Z < Size.Z; ++Z)
+                    {
+                        for (int32 Y = 0; Y < Size.Y; ++Y)
+                        {
+                            for (int32 X = 0; X < Size.X; ++X)
+                            {
+                                int32 Index = X + Y * Size.X + Z * Size.X * Size.Y;
+                                FFloat16Color Color = ColorData[Index];
+                                FVector Velocity(Color.R.GetFloat(), Color.G.GetFloat(), Color.B.GetFloat());
+                                WindGrid->SetCell(X, Y, Z, Velocity);
+                            }
+                        }
+                    }
+
+                    RHICmdList.UnlockBuffer(RDGReadbackBuffer->GetRHI());
+                }
+                else
+                {
+                    UE_LOG(LogTemp, Error, TEXT("Failed to lock readback buffer"));
+                }
+            }
+        );
+    }
+    GraphBuilder.Execute();
 }
 
 void UWindGPUSimulationComponent::Execute(UTextureRenderTargetVolume* InVelocityRenderTarget, UTextureRenderTargetVolume* InDensityRenderTarget, float DeltaTime)
