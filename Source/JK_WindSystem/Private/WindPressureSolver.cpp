1ff5067df5a4a808beeb33ef71a0bf62c382ab5d
Nguyen Phi Hung <josephkirk.art@gmail.com>
2025-07-21 09:39:34 +0700

    fix relative path for shader, fix pressure solver

--- Source/JK_WindSystem/Private/WindPressureSolver.cpp
+++ Source/JK_WindSystem/Private/WindPressureSolver.cpp
@@ -4,5 +4,5 @@
 // This is what links the C++ class to the shader file.
 // The first parameter is the C++ class name.
 // The second is the path to the shader file relative to the /Engine/Shaders/ or your project's /Shaders/ directory.
-IMPLEMENT_GLOBAL_SHADER(FWindPressureSolveCS, "/Shaders/Private/WindPressureSolveCS.usf", "MainCS", SF_Compute);
-IMPLEMENT_GLOBAL_SHADER(FWindPressureApplyCS, "/Shaders/Private/WindPressureApplyCS.usf", "MainCS", SF_Compute);
+IMPLEMENT_GLOBAL_SHADER(FWindPressureSolveCS, "/JK_WindSystem/WindPressureSolveCS.usf", "MainCS", SF_Compute);
+IMPLEMENT_GLOBAL_SHADER(FWindPressureApplyCS, "/JK_WindSystem/WindPressureApplyCS.usf", "MainCS", SF_Compute);
